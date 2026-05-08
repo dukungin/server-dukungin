d6a02d054be05c7ac592c8605f66d2e6f7a7bd0d
dukungin <dukung.in.support@gmail.com>
2026-05-08 13:26:11 +0700

    ok

--- routers/authRouter.js
+++ routers/authRouter.js
@@ -5,9 +5,5 @@ const authCtrl = require('../controllers/authController');
 
 router.post('/register',         authCtrl.register);
 router.post('/login',            authCtrl.login);
-router.post('/verify-pin',       authCtrl.verifyPin);
-router.post('/resend-pin',       authCtrl.resendPin);
-router.post('/forgot-password',  authCtrl.requestResetPassword);
-router.post('/reset-password',   authCtrl.resetPassword);
 
 module.exports = router;
