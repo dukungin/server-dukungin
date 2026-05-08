0e6180ac30309b847ef6133fb277569f794a4e83
dukungin <dukung.in.support@gmail.com>
2026-05-08 16:26:43 +0700

    fix req

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -3,6 +3,7 @@ require('dotenv').config();
 
 module.exports = (req, res, next) => {
   const token = req.headers['authorization']?.split(' ')[1];
+  console.log('req', req)
   if (!token) return res.status(403).json({ message: "Token diperlukan" });
 
   jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
