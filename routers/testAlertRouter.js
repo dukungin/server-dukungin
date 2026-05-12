095e991effe4a8f4e12a6281c89ae90040221217
dukungin <dukung.in.support@gmail.com>
2026-05-12 21:03:29 +0700

    api my donations

--- routers/testAlertRouter.js
+++ routers/testAlertRouter.js
@@ -2,9 +2,8 @@ const express = require('express');
 const router = express.Router();
 const testAlertCtrl = require('../controllers/testAlertController');
 const authMiddleware = require('../middleware/authMiddleware');
-const superAdminMiddleware = require('../middleware/superAdminMiddleware');
 
 // Hanya Super Admin yang boleh kirim test alert
-router.post('/send', authMiddleware, superAdminMiddleware, testAlertCtrl.sendInstantTestAlert);
+router.post('/send', authMiddleware, testAlertCtrl.sendInstantTestAlert);
 
 module.exports = router;
