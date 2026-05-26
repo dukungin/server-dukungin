564aefe6c655d3fb8096a95a03dba8b2bba2da34
dukungin <dukung.in.support@gmail.com>
2026-05-26 18:18:18 +0700

    fix ratelimiter create invoice

--- routers/midtransRouter.js
+++ routers/midtransRouter.js
@@ -41,7 +41,7 @@ const upload = multer({
 });
 
 // ─── Donasi ───────────────────────────────────────────────────────────────────
-router.post('/create-invoice', authMiddleware, midtransCtrl.createDonation);
+router.post('/create-invoice', midtransCtrl.createDonation);
 router.post('/webhooks',       midtransCtrl.handleWebhook);
 
 // ─── Withdrawal (Streamer) ────────────────────────────────────────────────────
